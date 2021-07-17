6e13287efc1012a3f88d04446aac200759668107
amit160000 <87486595+amit160000@users.noreply.github.com>
2021-07-17 21:46:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ df = pd.DataFrame()
 @app.route('/')
 def home():
     return render_template('index.html')
-@app.route('/predict',methods=['POST'])
+@app.route('/predict',methods=['GET'])
 def predict():
     global df
     input_features = [int(x) for x in request.form.values()]
